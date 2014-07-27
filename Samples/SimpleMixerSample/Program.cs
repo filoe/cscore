67e7720f7d97c12493d5916c82bc4035f3c55944
filoe <florian.rosmann@gmail.com>
2014-07-27 13:05:37 +0200

    Added a sample which demonstrates how to create a simple mixer and how to use it in combination which a custom channel conversion matrix.

---
+++ Samples/SimpleMixerSample/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CSCore;
+using CSCore.Codecs;
+using CSCore.DSP;
+using CSCore.SoundOut;
+using CSCore.Streams;
+
+namespace SimpleMixerSample
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "This example will mix one audio file with \n"+
+                "two sine waves (300Hz and 700Hz).\n"+
+                "The 300Hz sine wave will play only on the right\n" +
+                "channel and the 700Hz sine wave only on the left channel."+
+                "\n\n\nPlease select the audio file!\n"
+                );
+
+            IWaveSource fileWaveSource = null;
+
+            do
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog()
+                {
+                    Title = "Select any file to mix into",
+                    Filter = CodecFactory.SupportedFilesFilterEn
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        fileWaveSource = CodecFactory.Instance.GetCodec(openFileDialog.FileName);
+                    }
+                    catch
+                    {
+                    }
+                }
+
+            } while (fileWaveSource == null);
+
+
+            const int mixerSampleRate = 44100; //44.1kHz
+
+            var mixer = new CSCore.Streams.SimpleMixer(2, mixerSampleRate) //output: stereo, 44,1kHz
+            {
+                FillWithZeros = true,
+                DivideResult = true //you may play around with this
+            };
+
+            var monoToLeftOnlyChannelMatrix = new ChannelMatrix(ChannelMask.SpeakerFrontCenter,
+                ChannelMask.SpeakerFrontLeft | ChannelMask.SpeakerFrontRight);
+            var monoToRightOnlyChannelMatrix = new ChannelMatrix(ChannelMask.SpeakerFrontCenter,
+                ChannelMask.SpeakerFrontLeft | ChannelMask.SpeakerFrontRight);
+
+            /*
+            * Set the channel conversion matrix. 
+            * The y-axis specifies the input. This in only one channel since the SineGenerator only uses one channel. 
+            * The x-axis specifies the output. There we have to use two channels since we want stereo output. 
+            * The first value on the x-axis specifies the volume of the left channel, the second value 
+            * on the x-axis specifies the volume of the right channel.
+            * 
+            * If we take look at the left only channel conversion matrix, we can see that we are mapping one channel (y-axis)
+            * to two channels (x-axis). The left channel receives a volume of 1.0 (which means 100%) and the right channel 
+            * receives a volume of 0.0 (which means 0.0% -> muted). 
+            */
+            monoToLeftOnlyChannelMatrix.SetMatrix(
+                new[,]
+                {
+                    {1.0f, 0.0f}
+                });
+
+            monoToRightOnlyChannelMatrix.SetMatrix(
+                new[,]
+                {
+                    {0.0f, 1.0f}
+                });
+
+            VolumeSource volumeSource1, volumeSource2;
+
+            //Add any sound track.
+            mixer.AddSource(
+                fileWaveSource
+                .ChangeSampleRate(mixerSampleRate)
+                .ToStereo()
+                .ToSampleSource());
+
+            //Add a 700Hz sine with a amplitude of 0.5 which plays only on the left channel.
+            mixer.AddSource(
+                new SineGenerator(700, 0.5, 0).ToWaveSource()
+                .AppendSource(x => new DmoChannelResampler(x, monoToLeftOnlyChannelMatrix, mixerSampleRate))
+                .AppendSource(x => new VolumeSource(x), out volumeSource1));
+
+            //Add a 300Hz sine with a amplitude of 0.5 which plays only on the right channel.
+            mixer.AddSource(
+                new SineGenerator(300, 0.5, 0).ToWaveSource()
+                .AppendSource(x => new DmoChannelResampler(x, monoToRightOnlyChannelMatrix, mixerSampleRate))
+                .AppendSource(x => new VolumeSource(x), out volumeSource2));
+
+            //Initialize the soundout with the mixer. 
+            var soundOut = new WasapiOut() { Latency = 200 }; //better use a quite high latency
+            soundOut.Initialize(mixer.ToWaveSource());
+            soundOut.Play();
+
+            //adjust the volume of the input signals (default value is 100%):
+            volumeSource1.Volume = 0.5f; //set the volume of the 700Hz sine to 50%
+            volumeSource2.Volume = 0.7f; //set the volume of the 300Hz sine to 70%
+
+            Console.ReadKey();
+
+            mixer.Dispose();
+            soundOut.Dispose();
+        }
+    }
+}
---
+++ Samples/SimpleMixerSample/SimpleMixer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using CSCore;
+
+namespace SimpleMixerSample
+{
+    public class SimpleMixer : ISampleSource
+    {
+        private readonly WaveFormat _waveFormat;
+        private readonly List<ISampleSource> _sampleSources = new List<ISampleSource>();
+        private readonly object _lockObj = new object();
+        private float[] _mixerBuffer;
+
+        public bool FillWithZeros { get; set; }
+
+        public bool DivideResult { get; set; }
+
+        public SimpleMixer(int channelCount, int sampleRate)
+        {
+            if(channelCount < 1)
+                throw new ArgumentOutOfRangeException("channelCount");
+            if(sampleRate < 1)
+                throw new ArgumentOutOfRangeException("sampleRate");
+
+            _waveFormat = new WaveFormat(sampleRate, 32, channelCount, AudioEncoding.IeeeFloat);
+        }
+
+        public void AddSource(ISampleSource source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if(source.WaveFormat.Channels != WaveFormat.Channels ||
+               source.WaveFormat.SampleRate != WaveFormat.SampleRate)
+                throw new ArgumentException("Invalid format.", "source");
+
+            lock (_lockObj)
+            {
+                if (!Contains(source))
+                    _sampleSources.Add(source);
+            }
+        }
+
+        public void RemoveSource(ISampleSource source)
+        {
+            //don't throw null ex here
+            lock (_lockObj)
+            {
+                if (Contains(source))
+                    _sampleSources.Remove(source);
+            }
+        }
+
+        public bool Contains(ISampleSource source)
+        {
+            if (source == null)
+                return false;
+            return _sampleSources.Contains(source);
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            int numberOfStoredSamples = 0;
+
+            if (count > 0 && _sampleSources.Count > 0)
+            {
+                lock (_lockObj)
+                {
+                    _mixerBuffer = _mixerBuffer.CheckBuffer(count);
+                    List<int> numberOfReadSamples = new List<int>();
+                    for (int m = _sampleSources.Count -1; m >= 0; m--)
+                    {
+                        var sampleSource = _sampleSources[m];
+                        int read = sampleSource.Read(_mixerBuffer, 0, count);
+                        for (int i = offset, n = 0; n < read; i++, n++)
+                        {
+                            if (numberOfStoredSamples < i)
+                                buffer[i] = _mixerBuffer[n];
+                            else
+                                buffer[i] += _mixerBuffer[n];
+                        }
+                        if (read > numberOfStoredSamples)
+                            numberOfStoredSamples = read;
+
+                        if (read > 0)
+                            numberOfReadSamples.Add(read);
+                        else
+                        {
+                            //raise event here
+                            RemoveSource(sampleSource); //remove the input to make sure that the event gets only raised once.
+                        }
+                    }
+
+                    if (DivideResult)
+                    {
+                        numberOfReadSamples.Sort();
+                        int currentOffset = offset;
+                        int remainingSources = numberOfReadSamples.Count;
+
+                        foreach (var readSamples in numberOfReadSamples)
+                        {
+                            if (remainingSources == 0)
+                                break;
+
+                            while (currentOffset < offset + readSamples)
+                            {
+                                buffer[currentOffset] /= remainingSources;
+                                buffer[currentOffset] = Math.Max(-1, Math.Min(1, buffer[currentOffset]));
+                                currentOffset++;
+                            }
+                            remainingSources--;
+                        }
+                    }
+                }
+            }
+
+            if (FillWithZeros && numberOfStoredSamples != count)
+            {
+                Array.Clear(
+                    buffer,
+                    Math.Max(offset + numberOfStoredSamples - 1, 0),
+                    count - numberOfStoredSamples);
+
+                return count;
+            }
+
+            return numberOfStoredSamples;
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get { return _waveFormat; }
+        }
+
+        public long Position
+        {
+            get { return 0; }
+            set
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public long Length
+        {
+            get { return 0; }
+        }
+
+        public void Dispose()
+        {
+            lock (_lockObj)
+            {
+                foreach (var sampleSource in _sampleSources)
+                {
+                    sampleSource.Dispose();
+                    _sampleSources.Remove(sampleSource);
+                }
+            }
+        }
+    }
+}
