f71ae1cbc048cb494fc56c2721f6a282d777d2a8
filoe <florian.rosmann@gmail.com>
2014-08-01 17:26:58 +0200

    Improved SoundInSource class: -Added xml comments -Added validation rules for the soundIn paramter.

--- CSCore/Streams/SoundInSource.cs
+++ CSCore/Streams/SoundInSource.cs
@@ -3,20 +3,29 @@ using System;
 
 namespace CSCore.Streams
 {
+    /// <summary>
+    /// Represents an implementation of the <see cref="IWaveSource"/> interface which provides the data provided by a specified <see cref="ISoundIn"/> object.
+    /// </summary>
     public class SoundInSource : IWaveSource
     {
         private ISoundIn _soundIn;
         private WriteableBufferingSource _buffer;
 
+        /// <summary>
+        /// Occurs when new data is available.
+        /// </summary>
         public event EventHandler<DataAvailableEventArgs> DataAvailable;
 
+        /// <summary>
+        /// Gets the underlying <see cref="ISoundIn"/> instance.
+        /// </summary>
         public ISoundIn SoundIn
         {
             get { return _soundIn; }
         }
 
         /// <summary>
-        /// Creates an new instance of SoundInSource with a default bufferSize of 5 seconds.
+        /// Initializes a new instance of the <see cref="SoundInSource"/> class with a default bufferSize of 5 seconds.
         /// </summary>
         /// <param name="soundIn">The soundIn which provides recorded data.</param>
         /// <remarks>
@@ -25,15 +34,15 @@ namespace CSCore.Streams
         /// For example, if the bufferSize is about 5 seconds big, data which got recorded 6 seconds ago, won't be available anymore.
         /// </remarks>
         public SoundInSource(ISoundIn soundIn)
-            : this(soundIn, soundIn.WaveFormat.BytesPerSecond * 5)
+            : this(ThrowIfSoundInNotInitialized(soundIn), soundIn.WaveFormat.BytesPerSecond * 5)
         {
         }
 
         /// <summary>
-        /// Creates an new instance of SoundInSource with a default bufferSize of 5 seconds.
+        /// Initializes a new instance of the <see cref="SoundInSource"/> class.
         /// </summary>
         /// <param name="soundIn">The soundIn which provides recorded data.</param>
-        /// <param name="bufferSize">Size of the buffer in bytes.</param>
+        /// <param name="bufferSize">Size of the internal buffer in bytes.</param>
         /// <remarks>
         /// Note that soundIn has to be already initialized.
         /// Note that old data ("old" gets specified by the bufferSize) gets overridden. 
@@ -43,7 +52,7 @@ namespace CSCore.Streams
         {
             if (soundIn == null)
                 throw new ArgumentNullException("soundIn");
-            //bufferSize gets validated by WriteableBufferingSource
+            ThrowIfSoundInNotInitialized(soundIn);
 
             _buffer = new WriteableBufferingSource(soundIn.WaveFormat, bufferSize);
             _buffer.FillWithZeros = false;
@@ -53,11 +62,26 @@ namespace CSCore.Streams
 
         private void OnDataAvailable(object sender, DataAvailableEventArgs e)
         {
-            int written = _buffer.Write(e.Data, 0, e.ByteCount);
+            _buffer.Write(e.Data, 0, e.ByteCount);
             if (e.ByteCount > 0 && DataAvailable != null)
                 DataAvailable(this, e);
         }
 
+        /// <summary>
+        ///     Reads a sequence of bytes from the internal stream which holds recorded data and advances the position within the stream by the
+        ///     number of bytes read.
+        /// </summary>
+        /// <param name="buffer">
+        ///     An array of bytes. When this method returns, the <paramref name="buffer" /> contains the specified
+        ///     byte array with the values between <paramref name="offset" /> and (<paramref name="offset" /> +
+        ///     <paramref name="count" /> - 1) replaced by the bytes read from the current source.
+        /// </param>
+        /// <param name="offset">
+        ///     The zero-based byte offset in the <paramref name="buffer" /> at which to begin storing the data
+        ///     read from the current stream.
+        /// </param>
+        /// <param name="count">The maximum number of bytes to read from the current source.</param>
+        /// <returns>The total number of bytes read into the buffer.</returns>
         public int Read(byte[] buffer, int offset, int count)
         {
             int read = _buffer.Read(buffer, offset, count);
@@ -68,11 +92,17 @@ namespace CSCore.Streams
             return FillWithZeros ? count : read;
         }
 
+        /// <summary>
+        ///     Gets the <see cref="IWaveStream.WaveFormat" /> of the recorded data.
+        /// </summary>
         public WaveFormat WaveFormat
         {
             get { return SoundIn.WaveFormat; }
         }
 
+        /// <summary>
+        ///     Gets or sets the current position in bytes. This property is currently not supported. 
+        /// </summary>
         public long Position
         {
             get
@@ -85,20 +115,35 @@ namespace CSCore.Streams
             }
         }
 
+        /// <summary>
+        /// Gets the length in bytes. This property is currently not supported.
+        /// </summary>
         public long Length
         {
             get { return 0; }
         }
 
+        /// <summary>
+        /// Gets or sets a value which indicates whether the <see cref="Read"/> method should always provide the requested amount of data.
+        /// For the case that the internal buffer can't offer the requested amount of data, the rest of the requested bytes will be filled up with zeros.
+        /// </summary>
         public bool FillWithZeros { get; set; }
 
         private bool _disposed;
+
+        /// <summary>
+        /// Disposes the <see cref="SoundInSource"/>.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Disposes the <see cref="SoundInSource"/>.
+        /// </summary>
+        /// <param name="disposing">Not used.</param>
         protected virtual void Dispose(bool disposing)
         {
             if (!_disposed)
@@ -118,9 +163,20 @@ namespace CSCore.Streams
             _disposed = true;
         }
 
+        /// <summary>
+        /// Destructor of the <see cref="SoundInSource"/> class which calls the <see cref="Dispose(bool)"/> method.
+        /// </summary>
         ~SoundInSource()
         {
             Dispose(false);
+        }
+
+        private static ISoundIn ThrowIfSoundInNotInitialized(ISoundIn soundIn)
+        {
+            if(soundIn == null || soundIn.WaveFormat == null)
+                throw new ArgumentException("The SoundIn has to be initialized. Make sure that the passed SoundIn is not null and provides the format of the recorded data.", "soundIn");
+
+            return soundIn;
         }
     }
 }
