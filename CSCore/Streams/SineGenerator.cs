631055cbf378e4eb7412f22fbda56cdf035a9c6f
filoe <florian.rosmann@gmail.com>
2014-08-30 16:14:11 +0200

    Updated the SineGenerator class.

--- CSCore/Streams/SineGenerator.cs
+++ CSCore/Streams/SineGenerator.cs
@@ -2,75 +2,137 @@
 
 namespace CSCore.Streams
 {
+    /// <summary>
+    /// Generates a sine wave.
+    /// </summary>
     public class SineGenerator : ISampleSource
     {
-        private double _frequency = 1000;
-        private double _amplitude = 0.5;
-        private double _phase = 0;
-
-        private WaveFormat _waveFormat;
-
         /// <summary>
-        /// 1000Hz, 0.5 Amplitude, 0.0 phase
+        /// Gets or sets the frequency of the sine wave.
         /// </summary>
-        public SineGenerator()
+        public double Frequency
         {
-            _waveFormat = new WaveFormat(44100, 32, 1, AudioEncoding.IeeeFloat);
+            get { return _frequency; }
+            set
+            {
+                if(value <= 0)
+                    throw new ArgumentOutOfRangeException("value");
+                _frequency = value;
+            }
         }
 
-        public SineGenerator(double frequency, double amplitude, double phase)
-            : this()
+        /// <summary>
+        /// Gets or sets the amplitude of the sine wave.
+        /// </summary>
+        public double Amplitude
         {
-            _frequency = frequency;
-            _amplitude = amplitude;
-            _phase = phase;
+            get { return _amplitude; }
+            set
+            {
+                if(value < 0 || value > 1)
+                    throw new ArgumentOutOfRangeException("value");
+                _amplitude = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the phase of the sine wave.
+        /// </summary>
+        public double Phase { get; set; }
+
+
+        private readonly WaveFormat _waveFormat;
+        private double _frequency;
+        private double _amplitude;
+
+        /// <summary>
+        /// 1000Hz, 0.5 amplitude, 0.0 phase
+        /// </summary>
+        public SineGenerator()
+            : this(1000, 0.5, 0)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SineGenerator"/> class.
+        /// </summary>
+        /// <param name="frequency">Specifies the frequency of the sine wave in Hz.</param>
+        /// <param name="amplitude">Specifies the amplitude of the sine wave. Use a value between 0 and 1.</param>
+        /// <param name="phase">Specifies the initial phase. Use a value between 0 and 1.</param>
+        public SineGenerator(double frequency, double amplitude, double phase)
+        {
+            if(frequency <= 0)
+                throw new ArgumentOutOfRangeException("frequency");
+            if(amplitude < 0 || amplitude > 1)
+                throw new ArgumentOutOfRangeException("amplitude");
+
+            Frequency = frequency;
+            Amplitude = amplitude;
+            Phase = phase;
+
+            _waveFormat = new WaveFormat(44100, 32, 1, AudioEncoding.IeeeFloat);
         }
 
         public int Read(float[] buffer, int offset, int count)
         {
-            if (_phase == 1f)
-                _phase = 0;
+            if (Phase > 1)
+                Phase = 0;
 
             for (int i = offset; i < count; i++)
             {
-                float sine = (float)(_amplitude * Math.Sin(_frequency * _phase * Math.PI * 2));
+                float sine = (float)(Amplitude * Math.Sin(Frequency * Phase * Math.PI * 2));
                 buffer[i] = sine;
 
-                _phase += (1.0 / WaveFormat.SampleRate);
+                Phase += (1.0 / WaveFormat.SampleRate);
             }
 
             return count;
         }
 
+        /// <summary>
+        ///     Gets the <see cref="IWaveStream.WaveFormat" /> of the waveform-audio data.
+        /// </summary>
         public WaveFormat WaveFormat
         {
             get { return _waveFormat; }
         }
 
+        /// <summary>
+        /// Not supported.
+        /// </summary>
         public long Position
         {
             get { return 0; }
             set { throw new InvalidOperationException(); }
         }
 
+        /// <summary>
+        /// Not supported.
+        /// </summary>
         public long Length
         {
             get { return 0; }
         }
 
+        /// <summary>
+        /// Not used.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Not used.
+        /// </summary>
+        /// <param name="disposing">Not used.</param>
         protected virtual void Dispose(bool disposing)
         {
             if (disposing)
             {
                 //dispose managed
             }
-            //keine ressourcen zum freigeben
         }
 
         ~SineGenerator()
