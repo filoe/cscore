807255b9150c977527506512cb2d66d95b3fe720
filoe <florian.rosmann@gmail.com>
2015-02-07 00:01:06 +0100

    Fixed issue #21.

---
+++ CSCore/IWaveAggregator.cs
@@ -0,0 +1,13 @@
+﻿﻿namespace CSCore
+ {
+     /// <summary>
+     ///     Defines the base interface for any <see cref="IWaveSource" /> aggregators.
+     /// </summary>
+     public interface IWaveAggregator : IWaveSource
+     {
+         /// <summary>
+         ///     Gets the underlying <see cref="IWaveSource" />.
+         /// </summary>
+         IWaveSource BaseStream { get; }
+     }
+ }
