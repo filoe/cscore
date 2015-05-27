3242e3dad4c84fca74b4fd56ec9399d8a99a2384
filoe <florian.rosmann@gmail.com>
2015-05-27 21:01:01 +0200

    Added missing files related to 8a8ff637.

--- CSCore/DSP/DmoResampler.cs
+++ CSCore/DSP/DmoResampler.cs
@@ -24,7 +24,7 @@ namespace CSCore.DSP
             if (source == null)
                 throw new ArgumentNullException("source");
 
-            var waveFormat = (CSCore.WaveFormat)source.WaveFormat.Clone();
+            var waveFormat = (WaveFormat)source.WaveFormat.Clone();
             waveFormat.SampleRate = destSampleRate;
             return waveFormat;
         }
@@ -33,11 +33,11 @@ namespace CSCore.DSP
         ///     Initializes a new instance of the <see cref="DmoResampler" /> class.
         /// </summary>
         /// <param name="source"><see cref="IWaveSource" /> which has to get resampled.</param>
-        /// <param name="destSampleRate">The new output samplerate specified in Hz.</param>
-        public DmoResampler(IWaveSource source, int destSampleRate)
+        /// <param name="destinationSampleRate">The new output samplerate specified in Hz.</param>
+        public DmoResampler(IWaveSource source, int destinationSampleRate)
             : this(
                 source,
-                GetWaveFormatWithChangedSampleRate(source, destSampleRate))
+                GetWaveFormatWithChangedSampleRate(source, destinationSampleRate))
         {
         }
 
---
+++ CSCore/SoundOut/MmInterop/WaveInCaps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CSCore.SoundOut;
+using CSCore.SoundOut.MMInterop;
+using System.Runtime.InteropServices;
+
+namespace CSCore.SoundIn
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    internal struct WaveInCaps
+    {
+        public readonly short wMid;
+        public readonly short wPid;
+        public readonly int DriverVersion;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public readonly string Name;
+
+        public readonly MmDeviceFormats Formats;
+        public readonly short Channels;
+        public readonly short wReserved1;
+
+        public WaveFormat[] GetSupportedFormats()
+        {
+            return SoundOut.MMInterop.Utils.SupportedFormatsFlagsToWaveFormats(Formats);
+        }
+    }
+}
