764568c89f2f15012967de33bff9443f53476e15
filoe <florian.rosmann@gmail.com>
2015-05-29 14:29:55 +0200

    Changed assembly and file version to 1.1 with automatic build number and revision.

--- CSCore/Properties/AssemblyInfo.cs
+++ CSCore/Properties/AssemblyInfo.cs
@@ -15,14 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("7939b0f4-fed9-4dcf-bb59-a17505864c55")]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-// Hauptversion Nebenversion Buildnummer Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern übernehmen,
-// indem Sie "*" eingeben: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.*")]
 
 [assembly:CLSCompliant(true)]
 [assembly:InternalsVisibleTo("CSCore.Test")]
