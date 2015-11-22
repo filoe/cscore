065643b5846e085fd86c6c9ed07fcf23ddc72a6c
filoe <florian.rosmann@gmail.com>
2015-11-22 18:58:13 +0100

    WinformsVisualization: Fixed a potential IndexOutOfRangeException.

--- Samples/WinformsVisualization/Visualization/GradientCalculator.cs
+++ Samples/WinformsVisualization/Visualization/GradientCalculator.cs
@@ -29,6 +29,9 @@ namespace WinformsVisualization.Visualization
             {
                 int index = Convert.ToInt32((_colors.Length - 1) * perc - 0.5f);
                 float upperIntensity = (perc % (1f / (_colors.Length - 1))) * (_colors.Length - 1);
+                if (index + 1 >= Colors.Length)
+                    index = Colors.Length - 2;
+
                 return Color.FromArgb(
                     255,
                     (byte) (_colors[index + 1].R * upperIntensity + _colors[index].R * (1f - upperIntensity)),
