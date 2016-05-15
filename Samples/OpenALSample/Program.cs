8f226807f4228083388c1b3894fa4555ad291892
Patrick Yates <frozen.dinosaur@gmail.com>
2016-05-15 17:42:21 +1000

    Add a sample OpenAL commandline application.
    This succesfully compiles and runs on my Linux machine

---
+++ Samples/OpenALSample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using CSCore;
+using CSCore.SoundOut;
+using CSCore.Codecs.WAV;
+using System.Threading;
+using System.Linq;
+
+namespace OpenALSample
+{
+	class MainClass
+	{
+		static ISoundOut _soundOut;
+		static IWaveSource _waveSource;
+		public static void Main (string[] args)
+		{
+			string path = "";
+			if (args.Length > 0) path = args [0];
+			else
+			{
+				Console.WriteLine ("Enter the wave file to play:");
+				path = Console.ReadLine();
+			}
+
+			_waveSource = new WaveFileReader (path);
+			_soundOut = new ALSoundOut ();
+			_soundOut.Initialize (_waveSource);
+
+			_soundOut.Play ();
+
+			while (true)
+			{
+				if (Console.KeyAvailable)
+				{
+					var key = Console.ReadKey ();
+					if (key.Key == ConsoleKey.Escape)
+						break;
+				}
+
+				var str = String.Format (@"{0:mm\:ss\.f}/{1:mm\:ss\.f}",
+					          TimeConverterFactory.Instance.GetTimeConverterForSource (_waveSource)
+					.ToTimeSpan (_waveSource.WaveFormat, _waveSource.Position),
+					          TimeConverterFactory.Instance.GetTimeConverterForSource (_waveSource)
+					.ToTimeSpan (_waveSource.WaveFormat, _waveSource.Length));
+				str += String.Concat (Enumerable.Repeat (" ", Console.BufferWidth - 1 - str.Length));
+				Console.SetCursorPosition (0, Console.CursorTop);
+				Console.Write (str);
+				Thread.Sleep (100);
+			}
+			_soundOut.Stop ();
+			_soundOut.Dispose ();
+			_waveSource.Dispose ();
+			CSCore.SoundOut.AL.ALDevice.DefaultDevice.Dispose ();
+		}
+	}
+}
