21286bb8893b7f767b11642c8d359294b0bbbf7d
filoe <florian.rosmann@gmail.com>
2016-07-09 19:26:30 +0200

    Fixed FfmpegSample

--- Samples/FfmpegSample/Program.cs
+++ Samples/FfmpegSample/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using CSCore;
 using CSCore.Ffmpeg;
-using CSCore.SoundOut;
 
 namespace FfmpegSample
 {
@@ -15,7 +11,7 @@ namespace FfmpegSample
         {
             const string filename = @"D:\Temp\ffmpegtest\mp3.mp3";
             File.Delete("out.wav");
-            var source = new FfmpegDecoder(filename);
+            var source = new FfmpegDecoder(new Uri(filename));
             source.WriteToFile("out.wav");
         }
     }
