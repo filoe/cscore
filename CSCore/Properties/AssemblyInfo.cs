965c7b9dac6d9df7af6d7047f8ee7febc4db8afb
filoe <florian.rosmann@gmail.com>
2017-04-16 09:52:49 +0200

    MediaFoundationDecoder does not dispose underlying stream properly. See #230.

--- CSCore/Properties/AssemblyInfo.cs
+++ CSCore/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("7939b0f4-fed9-4dcf-bb59-a17505864c55")]
 
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.1.1")]
+[assembly: AssemblyFileVersion("1.2.1.1")]
 
 [assembly:CLSCompliant(true)]
 [assembly: InternalsVisibleTo("CSCore.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100237314800493cdf9aabec35955e8928e3d5416ad1d223e8914e0e025ff9095b21bbb696235b9d3886b0edec26107ca0af49c3170fc08d117e8e9265ab371b157f2c2b27843d97c1d312850d10d1272c1d46d18f02ac56f46676cbe7946049b1b344db7154d35788fee27b3d581bd7d43e41813b10fd360a3fbfab9199d9e86a4")]
