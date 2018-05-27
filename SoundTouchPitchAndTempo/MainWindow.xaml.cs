6fccb981e4b8deb7c754859b691b7fda0336413e
Andrew Zümwalt <ArizonaInkStudios@gmail.com>
2018-05-27 08:46:15 -0500

    Fixed a couple missed items when moving this to the Samples project.

--- SoundTouchPitchAndTempo/MainWindow.xaml.cs
+++ SoundTouchPitchAndTempo/MainWindow.xaml.cs
@@ -4,8 +4,6 @@ namespace SoundTouchPitchAndTempo
 {
     public partial class MainWindow : Window
     {
-        private IMainWindowViewModel _mainWindowViewModel;
-
         public MainWindow()
             : this(new MainWindowViewModel())
         {
